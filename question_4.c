26ef9bf9430bbdbebdc5fc14a5cebf0aeb0e7b4c
Chia Tu <74300658+chiia0904@users.noreply.github.com>
2021-04-26 19:13:49 +0800

    Add files via upload

---
+++ question_4.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+int printSeat(int *seating);
+int bubbleSort(int *seating);
+
+int main(){
+
+    int seating[] = {};
+    int i = 0, index = 0 ;
+    int input = 0;
+
+    printf("\n*Please input seating with 10 seat*\n");
+    for (i = 0; i < 10; i++){ //將輸入值存入陣列
+        scanf("%[^\n]d",&input);
+        seating[i] = input;
+    }
+    /*呼叫函式*/
+    printSeat(seating);
+    bubbleSort(seating);
+    printSeat(seating);
+
+    /*使迴圈重複執行*/
+    while(input != 0){
+
+        printf("\n*Please input seating with 10 seat*\n");
+
+        for (i = 0; i < 10; i++){
+            scanf("%[^\n]d",&input);
+            seating[i] = input;
+        }
+
+        printSeat(seating);
+        bubbleSort(seating);
+        printSeat(seating);
+    }
+}
+
+
+int bubbleSort(int *seating){
+
+    int i = 0, j = 0, temp;
+
+    /*bubble sort*/
+    for (i = 0; i < 10; i++){//外迴圈從index = 0 開始跑，內迴圈為i+1
+        for (j = i + 1; j < 10; j++){
+            if (seating[j] == 0){//遇到0就跳過
+                continue;
+            }
+            if (seating[i] > seating[j]){//如果前面數字小於後面，進行swap
+                temp = seating[i];
+                seating[i] = seating[j];
+                seating[j] = temp;
+            }
+        }
+    }
+    printf("*sorted seating*\n");
+    printSeat(seating);
+
+}
+
+int printSeat(int *seating){ //印出陣列
+
+    int i = 0;
+
+    for (i = 0; i < 10 ; i++){
+        printf("%d ", *seating);
+        seating++;
+    }
+    puts("");
+    printf("**********");
+}
